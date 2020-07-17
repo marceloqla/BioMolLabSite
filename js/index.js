9f41bddf288a7515d1cf0030640675da9a9bb2fd
marceloqla <marceloqla@gmail.com>
2020-07-17 09:30:39 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,8 +3,8 @@
 /* Imports */
 import * as THREE from '/node_modules/three/src/Three.js';
 // import * as THREE from '/js/three.js';
-import { GLTFLoader } from './GLTFLoader.js';
-import { OrbitControls } from './OrbitControls.js';
+import { GLTFLoader } from '/js/GLTFLoader.js';
+import { OrbitControls } from '/js/OrbitControls.js';
 
 /**
  * Variables and Events related to three.js Laboratory Scene
